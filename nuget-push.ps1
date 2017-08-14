1d3dee2661bcfb1626b5f5d19e190d112b628de1
bradphelan <b.phelan@weingartner.com>
2017-08-14 12:02:36 +0200

    bump verson

--- nuget-push.ps1
+++ nuget-push.ps1
@@ -1,4 +1,4 @@
-param([string]$apikey, [string]$configuration="Debug") 
+param([string]$apikey, [string]$configuration="Release") 
 rm *.nupkg
 .\NuGet.exe pack -Prop Platform=AnyCPU -Prop Configuration=$configuration -symbols .\ReactiveCompositeCollections\ReactiveCompositeCollections.csproj
 $pkg = ls .\*.nupkg | where { !$_.FullName.Contains(".symbols.") } | select-object -last 1
