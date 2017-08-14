0185b13c598794149433a5042a6a3554a7b0bb17
bradphelan <b.phelan@weingartner.com>
2017-08-14 12:38:45 +0200

    Upgrade nuget process to use nugetizer

--- nuget-push.ps1
+++ nuget-push.ps1
@@ -1,5 +1,13 @@
 param([string]$apikey, [string]$configuration="Release") 
-rm *.nupkg
-.\NuGet.exe pack -Prop Platform=AnyCPU -Prop Configuration=$configuration -symbols .\ReactiveCompositeCollections\ReactiveCompositeCollections.csproj
-$pkg = ls .\*.nupkg | where { !$_.FullName.Contains(".symbols.") } | select-object -last 1
-./nuget.exe push $pkg.FullName  -ApiKey $apikey
+param([string]$apikey) 
+
+# Remove any old nuget packages
+gci -r -include bin, obj | rm -rec -fo
+# Build new nuget packages
+msbuild /t:ReactiveCompositeCollections:Pack /p:Configuration=Release
+# Get all nuget packages under the specific folders
+$packages = gci -r -include *.nupkg -path ReactiveCompositeCollections
+# Publish them all
+foreach ($package in $packages) {
+    & "C:\Program Files\dotnet\dotnet.exe" nuget push $package
+}
