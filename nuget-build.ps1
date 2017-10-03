6879dc3f9da25fb8670959b58185ed155da8d42c
bradphelan <b.phelan@weingartner.com>
2017-10-03 10:24:36 +0200

    .\nuget-build.ps1

---
+++ nuget-build.ps1
@@ -0,0 +1,15 @@
+param([string]$apikey) 
+
+
+# Remove any old nuget packages
+gci -r -include bin, obj | rm -rec -fo
+# Build new nuget packages
+dotnet restore
+dotnet pack /p:Version=$(gitversion /output json /showvariable FullSemVer) --configuration Release
+# Get all nuget packages under the specific folders
+$packages = gci -r -include *.nupkg 
+# Publish them all
+foreach ($package in $packages) {
+    & dotnet nuget push $package
+}
+
